5e19735fd05ad163c578ff14c9f5f51de3111861
Johan-S <johstraat@gmail.com>
2024-06-08 17:12:41 +0200

    Created scene subsystem with window.

---
+++ Assets/Libs/Subsystems/Editor/SceneSubsystemWindow.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class SceneSubsystemWindow : EditorWindow {
+   [MenuItem("Nightfall/Scene Subsystem")]
+   private static void ShowWindow() {
+      var window = GetWindow<SceneSubsystemWindow>();
+      window.titleContent = new GUIContent("Scene Subsystem");
+      window.Show();
+   }
+
+   static bool is_open;
+
+   static bool in_window;
+
+
+   [CustomEditor(typeof(SceneSubsystem))]
+   public class OpeneSceneSubsystemWindow : Editor {
+      public override void OnInspectorGUI() {
+         if (!in_window) {
+            using (new EditorGUI.DisabledScope(is_open)) {
+               if (GUILayout.Button("Open Subsystem Window")) {
+                  ShowWindow();
+               }
+            }
+         }
+
+         base.OnInspectorGUI();
+      }
+   }
+
+
+   public SceneSubsystem subsystem;
+   public Editor editor;
+
+   void OnEnable() {
+      is_open = true;
+      Refresh();
+   }
+
+   void OnDisable() {
+      is_open = false;
+   }
+
+   void Update() {
+      if (SceneSubsystem.dirty) {
+         if (Refresh()) {
+            this.Repaint();
+         }
+      }
+   }
+
+   bool Refresh() {
+      SceneSubsystem.dirty = false;
+
+      if (editor is not null) {
+         if (!editor.serializedObject.targetObject) {
+            editor = null;
+         }
+      }
+
+      if (subsystem && subsystem.isActiveAndEnabled && subsystem.gameObject.scene.IsValid() &&
+          subsystem.gameObject.scene == EditorSceneManager.GetActiveScene()) {
+         // Debug.Log("No refresh");
+         if (!editor) {
+            editor = Editor.CreateEditor(subsystem);
+            // Debug.Log("But recreate editor!");
+         }
+
+         return false;
+      }
+
+      subsystem = FindObjectOfType<SceneSubsystem>();
+      if (subsystem && subsystem.gameObject.scene != EditorSceneManager.GetActiveScene()) {
+         subsystem = null;
+      }
+
+      if (subsystem) editor = Editor.CreateEditor(subsystem);
+      else {
+         editor = null;
+      }
+
+      // Debug.Log("Refresh");
+      return true;
+   }
+
+   private void OnGUI() {
+      in_window = true;
+      try {
+         if (editor is not null) {
+            if (!editor.serializedObject.targetObject) {
+               Debug.Log("Destroyed!");
+               // Refresh();
+            }
+         }
+
+         if (SceneSubsystem.dirty) {
+            Refresh();
+         }
+
+         if (editor) editor.OnInspectorGUI();
+      }
+      finally {
+         in_window = false;
+      }
+   }
+}
---
+++ Assets/Libs/Subsystems/SceneSubsystem.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+[DefaultExecutionOrder(-2000)]
+[ExecuteAlways]
+public class SceneSubsystem : MonoBehaviour {
+   static Dictionary<Scene, SceneSubsystem> _subsystems = new();
+
+   public void _Deactivate(GameObject o) {
+      o.SetActive(false);
+   }
+
+#if UNITY_EDITOR
+   public static bool dirty;
+
+   void OnValidate() {
+      dirty = true;
+   }
+#endif
+   static void MakeDirty() {
+#if UNITY_EDITOR
+      dirty = true;
+#endif
+   }
+
+
+   public UnityEvent beforeAwake;
+   public UnityEvent beforeStart;
+   public UnityEvent onUpdate;
+
+   public UnityEvent onLateUpdate;
+
+   void Awake() {
+      if (Application.isEditor) return;
+      var s = gameObject.scene;
+
+      if (_subsystems.TryGetValue(s, out var v)) {
+         if (v) {
+            Debug.Log($"duplicate scene subsystem! {v} and {this}");
+            enabled = false;
+            return;
+         }
+      }
+
+      beforeAwake?.Invoke();
+   }
+
+   void OnEnable() {
+      dirty = true;
+   }
+
+   void OnDisable() {
+      dirty = true;
+   }
+
+   // Start is called before the first frame update
+   void Start() {
+      if (Application.isEditor) return;
+      beforeStart?.Invoke();
+   }
+
+   // Update is called once per frame
+   void Update() {
+      if (Application.isEditor) return;
+      onUpdate?.Invoke();
+   }
+
+   void LateUpdate() {
+      if (Application.isEditor) return;
+      onLateUpdate?.Invoke();
+   }
+}
