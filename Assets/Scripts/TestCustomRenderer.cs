f1da6e131bee0662dd14c96e6239860693d354ab
Johan-S <johstraat@gmail.com>
2024-06-26 22:42:18 +0200

    Format.

--- Assets/Scripts/TestCustomRenderer.cs
+++ Assets/Scripts/TestCustomRenderer.cs
@@ -6,31 +6,30 @@ using UnityEngine;
 public class TestCustomRenderer : MonoBehaviour {
    public Mesh mesh;
 
-    static Material lineMaterial;
-    static void CreateLineMaterial()
-    {
-        if (!lineMaterial)
-        {
-            // Unity has a built-in shader that is useful for drawing
-            // simple colored things.
-            Shader shader = Shader.Find("Hidden/Internal-Colored");
-            lineMaterial = new Material(shader);
-            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
-            // Turn on alpha blending
-            lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-            lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-            // Turn backface culling off
-            lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
-            // Turn off depth writes
-            lineMaterial.SetInt("_ZWrite", 0);
-        }
-    }
+   static Material lineMaterial;
+
+   static void CreateLineMaterial() {
+      if (!lineMaterial) {
+         // Unity has a built-in shader that is useful for drawing
+         // simple colored things.
+         Shader shader = Shader.Find("Hidden/Internal-Colored");
+         lineMaterial = new Material(shader);
+         lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+         // Turn on alpha blending
+         lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+         lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+         // Turn backface culling off
+         lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+         // Turn off depth writes
+         lineMaterial.SetInt("_ZWrite", 0);
+      }
+   }
 
 
    private void OnRenderObject() {
       // Debug.Log($"on render {Camera.current.name}");
       CreateLineMaterial();
-      
+
       // Not sure this check is needed?
       if (
          // Game view.
@@ -39,7 +38,6 @@ public class TestCustomRenderer : MonoBehaviour {
          || Camera.current.name == "SceneCamera"
          // Renders the camera preview when you select a camera. Note the space.
          || Camera.current.name == "Preview Camera") {
-
          // Apply the material
          lineMaterial.SetPass(0);
 
