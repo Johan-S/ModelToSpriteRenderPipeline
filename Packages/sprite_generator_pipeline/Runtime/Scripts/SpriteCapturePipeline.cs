a7f8c1c2c690478da4eaa97a06cff158464deeb0
Johan-S <johstraat@gmail.com>
2024-07-03 07:35:12 +0200

    Remove unused.

--- Packages/sprite_generator_pipeline/Runtime/Scripts/SpriteCapturePipeline.cs
+++ Packages/sprite_generator_pipeline/Runtime/Scripts/SpriteCapturePipeline.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.Profiling;
 using UnityEngine;
-using UnityEngine.Rendering.VirtualTexturing;
 using static UnityEngine.Mathf;
 using FilterMode = UnityEngine.FilterMode;
 
